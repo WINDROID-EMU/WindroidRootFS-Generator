351b93fc6de11b70530b58fb76d52854f5d66658
A CASA DO EMULADOR <45230455+WINDROID-EMU@users.noreply.github.com>
2025-07-11 03:30:55 -0300

    Wine
    Implementa sistema de Memory Pooling no driver Android (wineandroid.drv)

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -5,7 +5,7 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-#GIT_COMMIT=5db8b28085fa9d16c98757fb8309df4f94c41e77
+GIT_COMMIT=8f74d79421705352e0d80506731ce06a7cc12b9c
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
