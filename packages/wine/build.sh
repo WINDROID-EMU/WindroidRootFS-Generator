e993427432a606649e84101face9e499f57e0609
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-07-10 22:05:30 -0300

    Wine
    dlls dinput dinput8 e xinput fix

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -4,8 +4,8 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCH=aarch64
 
-GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=1a4492046555dd13ddee5d7f12fa29f9ed47b697
+GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
+GIT_COMMIT=5db8b28085fa9d16c98757fb8309df4f94c41e77
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
